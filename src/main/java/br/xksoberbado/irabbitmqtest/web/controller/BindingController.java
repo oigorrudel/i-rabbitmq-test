d51cb2ae392601809a6ef5ff0febd3994b3677cf
Igor Rudel <igor.rudel@realizecfi.com.br>
2025-06-19 15:37:20 -0300

    --

--- src/main/java/br/xksoberbado/irabbitmqtest/web/controller/BindingController.java
+++ src/main/java/br/xksoberbado/irabbitmqtest/web/controller/BindingController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,10 +23,13 @@ public class BindingController {
     private final AmqpAdmin amqpAdmin;
     private final ExchangeFactory exchangeFactory;
 
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String vhost;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody @Valid final BindingBody body) {
-        final var exchange = client.getExchange("my-vhost", body.exchange());
+        final var exchange = client.getExchange(vhost, body.exchange());
 
         amqpAdmin.declareBinding(
             BindingBuilder
