46b5ef02103f10455d738fab0ad24c20558db3a4
Igor Rudel <igor.rudel@realizecfi.com.br>
2025-06-23 19:00:05 -0300

    --

--- src/main/java/br/xksoberbado/irabbitmqtest/client/RabbitMQAPIClient.java
+++ src/main/java/br/xksoberbado/irabbitmqtest/client/RabbitMQAPIClient.java
@@ -5,13 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
 @FeignClient(name = "rabbit-mq", url = "${feign.client.rabbitmq-api.url}")
 public interface RabbitMQAPIClient {
-
-    @GetMapping("exchanges")
-    List<Object> getExchanges();
 
     @GetMapping("exchanges/{vhost}/{name}")
     RabbitMQExchangeResponse getExchange(@PathVariable String vhost,
---
+++ src/main/java/br/xksoberbado/irabbitmqtest/config/CustomGatewayConfig.java
@@ -0,0 +1,34 @@
+package br.xksoberbado.irabbitmqtest.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.util.Base64;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.*;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class CustomGatewayConfig {
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Bean
+    public RouterFunction<ServerResponse> gatewayRoutes() {
+        return route("rabbitmq-api")
+            .GET("/rabbitmq-api/**", http())
+            .before(rewritePath("/rabbitmq-api/(?<segment>.*)", "/api/${segment}"))
+            .before(uri("http://localhost:15672"))
+            .before(addRequestHeader(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes())))
+            .build();
+    }
+}
